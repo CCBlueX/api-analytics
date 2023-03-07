4431c7a72b542d0dddc67fa8f23a8e03de219879
1zuna <marco@ccbluex.net>
2023-03-07 03:33:37 +0100

    added content type header

--- dashboard/server.js
+++ dashboard/server.js
@@ -9,6 +9,7 @@ server.use(express.static(path.join(__dirname, "public")));
 server.get("*", function(req, res) {
   const { html } = app.render({ url: req.url });
 
+  res.setHeader('Content-Type', 'text/html');
   res.write(`
     <!DOCTYPE html>
     <link rel='stylesheet' href='/global.css'>
